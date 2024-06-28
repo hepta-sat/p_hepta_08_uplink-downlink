4f7011a882b402cb528ba95670fe6dcd1a9d26fc
sonenagisa <csna20061@g.nihon-u.ac.jp>
2024-06-28 02:05:06 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include "mbed.h"
+#include "HEPTA_COM.h"
 
-DigitalOut myled(LED1);
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_COM com(p9,p10,9600);
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+int main(){
+    pc.printf("Xbee communication Mode\r\n");
+    int rcmd=0,cmdflag=0;
+    for(int i = 0; i < 50; i++) {
+        com.printf("num = %d\r\n",i);
+        com.xbee_receive(&rcmd,&cmdflag);
+        pc.printf("rcmd=%d, cmdflag=%d\r\n",rcmd, cmdflag);
+        if (cmdflag == 1) {
+            if (rcmd == 'a') {
+                pc.printf("Command Get %d\r\n",rcmd);
+                com.printf("HEPTA Uplink OK\r\n");
+            }
+            com.initialize();
+        }
+        wait(1.0);
+    }
 }
